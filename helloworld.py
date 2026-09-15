11c93cdf1845fa34e3aec8f2cfb99d5107e23be5
Arshiya <arshiya.mostafavisabet@gmail.com>
2026-09-15 10:20:42 -0400

    added another print statement to write my name in the terminal

--- helloworld.py
+++ helloworld.py
@@ -2,6 +2,8 @@
 # Author: Arshiya MS
 # Date: 15/09/2026
 # Brief: The program prints the phrase "Hello World" in the terminal
+# Version: 1.1.0 -> added another print statement to include the author's name in print statement
 
 print("Hello World")
+print("Arshiya Mostafavisabet")
 
