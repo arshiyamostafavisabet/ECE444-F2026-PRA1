fd1f22a3b834963cf46f90a9ce92db94c9f8831c
Arshiya <arshiya.mostafavisabet@gmail.com>
2026-09-15 10:20:42 -0400

    added another print statement to write my name in the terminal

--- helloworld.py
+++ helloworld.py
@@ -2,6 +2,7 @@
 # Author: Arshiya MS
 # Date: 15/09/2026
 # Brief: The program prints the phrase "Hello World" in the terminal
+# Version: 1.1.0 -> added another print statement to include the author's name in print statement
 
 enrollmentYears = 3
 print(f"I have been at UofT for {enrollmentYears} years")
